27849aaa14d73e262f8a603d7c98ed99517346a7
Bogdan Bryzh <bogdan.bryzh@gmail.com>
2020-10-26 00:53:41 +0200

    preburn stage

--- 4.2/main.cpp
+++ 4.2/main.cpp
@@ -5,21 +5,27 @@ using namespace std;
 
 int main()
 {
-    double w, z;
+    double i, w, z, num;
 
-    cout << "Enter z: ";
-    cin >> z;
+    cout << "Enter number: ";
+    cin >> num;
 
-    if ((-2 <= z) && (z <= 0.5))
+    if ((-2 <= num) && (num <= 0.5))
     {
-        w = z - sin(z);
-    } else if ((0.5 <= z) && (z <= 3)) {
-        for ()
+        for (z = -2; z <= 0.5; z += 0.5)
         {
-            
+            cout << "z = " << z << endl;
+            cout << "w = " << (z - sin(z)) << endl;
         }
     }
-    
+    else if ((0.5 <= num) && (num <= 3))
+    {
+        for (z = 0.5; z <= 3; z += 0.5)
+        {
+            cout << "z = " << z << endl;
+            cout << "w = " << (z - sin(z)) << endl;
+        }
+    }
 
     return 0;
 }
